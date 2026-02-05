0b8bd5c055485d4d0e00e29bb5b9cb40dedca259
Alec Nono <alecbnono@gmail.com>
2026-02-05 12:14:07 +0800

    feat: change button to toggle group

--- client/app/features/reserve/components/atom/Timeslot.tsx
+++ client/app/features/reserve/components/atom/Timeslot.tsx
@@ -2,18 +2,30 @@ import { Button } from "~/components/ui/button";
 import type { TimeslotType } from "../../types/reserve.types";
 import { formatTime } from "../../utils/reserve";
 
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+
 export default function Timeslot({
     timeInMins,
     occupiedSeats,
     capacity,
 }: TimeslotType) {
     return (
-        <Button className="rounded-full p-4  bg-neutral-50 border-2 border-primary">
+        <ToggleGroupItem
+            value={timeInMins.toString()}
+            className="rounded-full p-4 bg-neutral-50 border-2 border-primary 
+            data-[state=on]:bg-primary 
+            data-[state=on]:text-neutral-50
+            hover:bg-green-200 
+            hover:text-neutral-950
+            transition
+            duration-150
+            "
+        >
             <span>{formatTime(timeInMins)}</span>
             <span>
                 {" "}
                 | {occupiedSeats}/{capacity}
             </span>
-        </Button>
+        </ToggleGroupItem>
     );
 }
--- client/app/features/reserve/components/organism/LaboratoryConfirmation.tsx
+++ client/app/features/reserve/components/organism/LaboratoryConfirmation.tsx
@@ -1,9 +1,14 @@
 import Timeslot from "../atom/Timeslot";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 export default function LaboratoryConfirmation() {
     return (
         <div className="flex justify-between p-8 grow items-center">
-            <div className="flex flex-col gap-2 max-h-125 flex-wrap">
+            <ToggleGroup
+                type="single"
+                className="flex flex-col gap-2 max-h-125 flex-wrap"
+                spacing={2}
+            >
                 {Array.from({ length: 22 }, (_, i) => i + 1).map((minutes) => (
                     <Timeslot
                         timeInMins={390 + minutes * 30}
@@ -11,7 +16,7 @@ export default function LaboratoryConfirmation() {
                         occupiedSeats={10}
                     />
                 ))}
-            </div>
+            </ToggleGroup>
         </div>
     );
 }
