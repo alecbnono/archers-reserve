fd9f2876357b6181bb91423362eed3565a80f143
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2026-03-27 04:54:04 +0800

    update client jest.config

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,10 +2,15 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+    moduleNameMapper: {
+        // This handles CSS/Asset imports which break Jest
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    },
     reporters: [
         "default",
-        ["./node_modules/jest-html-reporter", {
-            pageTitle: "Server side Test Report",
+        ["jest-html-reporter", { // Simplified path
+            pageTitle: "Client Side Test Report",
             includeFailureMsg: true,
             includeConsoleLog: true,
             sort: 'titleAsc'
